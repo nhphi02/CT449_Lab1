ebf9fe229c57df2813d049e60dab4a76a190bfb0
Nguyễn Hồng Phi <nguyenphi17022@gmail.com>
2024-03-08 11:58:31 +0700

    cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 // handle 404 response
 app.use((req, res, next) => {
     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
-    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    //      khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
     return next(new ApiError(404, "Resource not found"));
 });
 
